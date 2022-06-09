81b449cf66164a248519c78032ff1d0e2f0e9ad1
África Mayor Colomina <amayorcolomina@deloittedigital.es>
2022-06-09 10:09:28 +0200

    refactor

--- src/presentation/grocery/User/TabPromosList/TabPromosList.js
+++ src/presentation/grocery/User/TabPromosList/TabPromosList.js
@@ -2,8 +2,7 @@ import Spinner from 'presentation/components/Spinner/Spinner.vue';
 import PromosList from './PromosList/PromosList.vue';
 import { useI18n } from 'vue-i18n';
 import useCustomerTools from 'hooks/useCustomerTools';
-// import { computed, watch, ref } from 'vue';
-import { computed, ref, watch } from 'vue';
+import { computed } from 'vue';
 
 export default {
     components: {
@@ -13,49 +12,19 @@ export default {
     setup() {
         const { t } = useI18n();
         const tools = useCustomerTools();
-        const idsPromos = ref();
         const { customFields, loading } = tools.useCustomFieldsCustomer();
 
         const customFieldsNotEmpty = computed(() => {
             return Boolean(customFields.value);
         });
 
-        watch(customFieldsNotEmpty, (customFieldsNotEmpty) => {
-            if (customFieldsNotEmpty) {
+        const idsPromos = computed(() => {
+            if (customFieldsNotEmpty.value) {
                 const activePromos = customFields.value.find((field) => field.name === "promocionesActivas")?.value;
-                idsPromos.value = activePromos?.split(";");
+                return activePromos?.split(";");
             }
+            return [];
         });
-
-        // const { promos, loading } = tools.useMyPromos({ idsPromos, locale });
-        //     promosListNotEmpty.value = computed(() => {
-        //         return Boolean(promos.value.length);
-        //     });
-        //     _loading.value = computed(() => {
-        //         return Boolean(loading);
-        //     });
-        //     _promos.value = computed(() => {
-        //         return Object.assign({}, promos);
-        //     });
-
-        // watch(customFieldsNotEmpty, (customFieldsNotEmpty) => {
-        //     if (customFieldsNotEmpty) {
-        //         const activePromos = customFields.value.find((field) => field.name === "promocionesActivas")?.value;
-        //         const idsPromos = activePromos?.split(";");
-        //         if (idsPromos) {
-        //             const { promos, loading } = tools.useMyPromos({ idsPromos, locale });
-        //             promosListNotEmpty.value = computed(() => {
-        //                 return Boolean(promos.value.length);
-        //             });
-        //             _loading.value = computed(() => {
-        //                 return Boolean(loading);
-        //             });
-        //             _promos.value = computed(() => {
-        //                 return promos;
-        //             });
-        //         }
-        //     }
-        // });
 
         return {
             t,
