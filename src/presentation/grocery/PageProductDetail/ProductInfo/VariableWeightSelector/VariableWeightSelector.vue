e976ce245b30fe1fddaf37a5dfd4ed96a5e59c13
pmartinezlopez <pmartinezlopez@deloittedigital.es>
2022-06-09 12:44:09 +0200

    featur weight selector

---
+++ src/presentation/grocery/PageProductDetail/ProductInfo/VariableWeightSelector/VariableWeightSelector.vue
@@ -0,0 +1,15 @@
+<script src="./VariantSelector.js"></script>
+<i18n src="./VariantSelector.txt" lang="yaml"></i18n>
+
+<template>
+  <div class="row select-row">
+    <ul class="list-inline"
+        data-test="input-selector-weight">
+      <li>
+        <p>Introduzca el peso en gramos</p>
+        <input type="number" id="weightQty" value="1000" v-model="weightSelection" @change="selectedWeight">
+        <span> Gramos</span>
+      </li>
+    </ul>
+  </div>
+</template>
