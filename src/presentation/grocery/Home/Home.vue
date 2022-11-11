5c2a09021cf3394a102bf4538eef09e5140d3094
Marco <mpalaciosarauzo@deloitte.es>
2022-11-11 14:42:38 +0100

    Add url location language

--- src/presentation/grocery/Home/Home.vue
+++ src/presentation/grocery/Home/Home.vue
@@ -35,7 +35,7 @@
           <img class="card-img-top receitas"
             src="https://www.pingodoce.pt/wp-content/uploads/2022/10/receitas-one-pot.jpg"/>
           <div class="card-body pr-70 pl-70 pb-50 pt-50">
-            <a href="/products/receitas"> 
+            <a href="/PT/pt/products/receitas"> 
               <p class="card-text" style="color: #fefefe;"> O que lhe apetece comer hoje? Temos sugestões saudáveis e equilibradas para ajudar a planear os seus
               almoços e jantares
               </p>
